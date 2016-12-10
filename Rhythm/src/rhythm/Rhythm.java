e436e70318b36883039f48efc0b4740f2e59565b
trumpet63 <xtrumpet63x@gmail.com>
2016-12-10 15:51:01 -0500

    GitHub Working

---
+++ Rhythm/src/rhythm/Rhythm.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package rhythm;
+
+/**
+ *
+ * @author Owner
+ */
+public class Rhythm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
